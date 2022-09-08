87c86227d95e41485c845c1c3be33d06b6ce6c13
radiosignalz <93599478+radiosignalz@users.noreply.github.com>
2022-09-08 10:16:43 +0300

    Add files via upload

---
+++ HW7_Registry.php
@@ -0,0 +1,72 @@
+<?php
+//2. Найти в проекте паттерн Registry и объяснить, почему он был применён.
+
+
+// Объявлен класс  Реестра для дальнейшего использования в паттерне управления активными компонентами!
+// В частности вызов контейнера загрузки и установление зависимостей
+// получение параметров страницы для построения маршрутов,
+//
+//Соответственно обращение к реестру происходит где-то в точке входа index  и тд
+
+declare(strict_types = 1);
+
+namespace Framework;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+
+class Registry
+{
+    /**
+     * @var ContainerBuilder
+     */
+    private static $containerBuilder;
+
+    /**
+     * Добавляем контейнер для работы реестра
+     *
+     * @param ContainerBuilder $containerBuilder
+     * @return void
+     */
+    public static function addContainer(ContainerBuilder $containerBuilder): void
+    {
+        static::$containerBuilder = $containerBuilder;
+    }
+
+    /**
+     * Получаем данные из конфигурационного файла
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public static function getDataConfig(string $name)
+    {
+        if (!static::$containerBuilder->hasParameter($name)) {
+            throw new \InvalidArgumentException('Unknown config variable ' . $name);
+        }
+
+        return static::$containerBuilder->getParameter($name);
+    }
+
+    /**
+     * Рендерим страницу по названию роута
+     *
+     * @param string name
+     * @param array $parameters
+     * @return string
+     */
+    public static function getRoute(string $name, array $parameters = []): string
+    {
+        /** @var RouteCollection $routeCollection */
+        $routeCollection = static::$containerBuilder->get('route_collection');
+
+        $urlGenerator = new UrlGenerator($routeCollection, new RequestContext());
+        try {
+            return $urlGenerator->generate($name, $parameters);
+        } catch (\Exception $e) {
+            throw new \InvalidArgumentException('Unknown route name ' . $name);
+        }
+    }
+}
